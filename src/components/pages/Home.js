4d313da3be692b69207269b43eced008f38a737c
JonGudenzi <jongudenzi@gmail.com>
2021-05-09 12:41:54 -0400

    alignment of about me containers

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,8 @@ import bg from "../../assets/img/bg2.jpg";
 function Home() {
   return (
 
-    <div style={{ backgroundImage: `url(${bg})`, 
+    <div
+    style={{ backgroundImage: `url(${bg})`, 
     backgroundRepeat: 'no-repeat', 
     backgroundSize: 'cover',
     width: '100vw', 
@@ -16,10 +17,10 @@ function Home() {
         <img
           src={selfImg}
           alt="Jon Gudenzi"
-          className="selfImg "
+          className="selfImg"
         />
       
-      <p className="col-7 text-center aboutMe flexItems">
+      <p className="col-7 text-center aboutMe">
           I am an aspiring developer who currently works a full time job in inventory and logistics. I have been with my current employer for 15 years and have peaked at performance and growth within the organization. I've chosen a new career path in computer programming with a focus on web development because I enjoy the challenging and creative outlet.
       </p>
     
