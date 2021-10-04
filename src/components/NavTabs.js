2878c5371392006773d4080445c9c9ccd5cc8f6f
JonGudenzi <jongudenzi@gmail.com>
2021-10-03 23:03:34 -0400

    Adjustments to nav bar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,39 +1,59 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../index.css";
+import resume from "../assets/Resume.pdf";
+import { Nav, Navbar } from "react-bootstrap";
 
 
 function NavTabs() {
   const location = useLocation();
 
   return (
-    <div style={{backgroundColor: "black"}}>
-          <ul className="nav nav">
-              <h1 style={{color: "white"}} className="col-9 jonName">Jon Gudenzi</h1>
-      <li className="nav-item">
-        <Link style={{color: "white", fontSize: '21px'}} to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-      </li>
-      <li className="nav-item">
-        <Link style={{color: "white", fontSize: '21px'}}
-          to="/work"
-          className={location.pathname === "/work" ? "nav-link active" : "nav-link"}
-        >
-          Work
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link style={{color: "white", fontSize: '21px'}}
-          to="/contact"
-          className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </Link>
-      </li>
-    </ul>
+<div className='nav' style={{ backgroundColor: "black", display: "flex", flexwrap: "wrap"}}>
+<div className="nav-links">
+    <h1 style={{ color: "rgb(94, 240, 215)", textAlign: "left" }}>Jon Gudenzi</h1>
+    </div>
+
+    <Navbar className='nav-wrap'>
+        <Nav className="nav-item">
+          <Link style={{ color: "white", fontSize: '21px' }} to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+            Home
+          </Link>
+        </Nav>
+        
+        <Nav className="nav-item">
+          <Link style={{ color: "white", fontSize: '21px' }}
+            to="/work"
+            className={location.pathname === "/work" ? "nav-link active" : "nav-link"}
+          >
+            Work
+          </Link>
+        </Nav>
+        <Nav className="nav-item">
+          <Link style={{ color: "white", fontSize: '21px' }}
+            to="/contact"
+            className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+          >
+            Contact
+          </Link>
+          </Nav>
+          <Nav className="nav-item">
+          <Link style={{ color: "white", fontSize: '21px' }}
+to={resume}
+            className={
+              location.pathname === "/resume" ? "nav-link active" : "nav-link"
+            }
+            href={resume}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Resume
+
+          </Link>
+        </Nav>
+     
+    </Navbar>
+    
     </div>
   );
 }
