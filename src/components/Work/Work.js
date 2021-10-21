4fd23ea677d543217a2565e5eb6f84d37fa1e78b
JonGudenzi <jongudenzi@gmail.com>
2021-10-20 22:45:34 -0400

    Styling on work component.  Removed carousel, replace with cards

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -2,117 +2,62 @@ import React from "react";
 import WorkData from '../WorkData/WorkData';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
-import { Card } from "@material-ui/core";
+import {  Card, CardMedia, Button, CardActions, Typography, CardContent } from "@material-ui/core";
+import githubIcon from '../../assets/img/GitHub-Mark-120px-plus.png';
+
 import './Work.css';
+// import { Container } from "react-bootstrap";
 
 function Work() {
 
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 2000,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        initialSlide: 0,
-        arrows: true,
-        className: 'slides',
-        responsive: [
-            {
-                breakpoint: 600,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2,
-                  initialSlide: 2
-                }
-            },
-        {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1 
-        }
-    }]
-      };
+
 
     return (
-<div>
-        <div className="work" container justify = "center">
-            
+        <div>
+            <div className="work cardDiv" container justify="center">
+
                 <h1 className="col-12 text-center">Work</h1>
-            
-            <Slider {...settings}>
-
-                    {WorkData.map((data, index) => {
-                        return (
-                            
-                            <Card key={index} className="col-10 m-4">
-
-                                <div
-                                    style={{
-                                        display: 'flex',
-                                        flexWrap: "wrap",
-                                        justifyContent: "space-between",
-                                        
-                                        
-                                    }}>
-                                    <div className="row">
-                                        <img className="col-12 mb-3"
-                                            src={data.image}
-                                            alt={data.title}
-                                        />
-                                    </div>
-                                    <p
-                                        style={{
-                                            padding: "8px",
-                                            border: "2px black solid",
-                                            textAlign: "center",
-                                            display: 'flex',
-                                            flexWrap: "wrap"
-                                        }}>
-                                        {data.desc}
-                                    </p>
-
-                                    <div style={{
-                                        border: "2px black solid",
-                                        textAlign: "center",
-                                        display: "inline-block"
-                                    }}>
-
-                                        <a href={data.app}
-                                            target={data.app}>
-                                            <button className="title col-12"
-                                                style={{
-                                                    backgroundColor: "black",
-                                                    color: "white",
 
 
-                                                }}>
-                                                <h3>{data.title}</h3>
-                                                <p>{data.language}</p>
-                                            </button>
-                                        </a>
+               
 
-                                        <a href={data.github}
-                                            target={data.github}>
-                                            <button className="githubLink col-12"
-                                                style={{
-                                                    backgroundColor: "black",
-                                                    color: "white",
+                {WorkData.map((data, index) => {
+                    return (
 
-                                                }}>
-                                                <h3>Github repo</h3>
-                                            </button>
-                                        </a>
-                                    </div>
-                                </div>
-                            </Card>
-                            
-                        )
-                    })}
-                    
-                    </Slider>
-        </div>
+    
+                        <Card className="card" key={index}>
+
+                            <CardMedia
+                                component="img"
+                                image={data.image}
+                                alt={data.image}
+                                height="220"
+                            />
+
+                            <CardContent>
+                                <Typography gutterBottom variant="p" component="div">
+                                    {data.desc}
+                                </Typography>
+                            </CardContent>
+
+                            <CardActions>
+                                <Button size="large" href={data.app}>Application</Button>
+                                <link 
+                                rel={githubIcon}
+                                type="image/png"
+                                src="../../assets/img/GitHub-Mark-120px-plus.png"
+                                 href={data.github} />
+                            </CardActions>
+
+
+                        </Card>
+                       
+                        
+                    )
+                })}
+
+
+            </div>
         </div>
     )
 }
