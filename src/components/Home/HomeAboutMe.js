d6119a4be0902151da2c1c788330acf3f35f3579
JonGudenzi <jongudenzi@gmail.com>
2022-10-05 22:14:49 -0400

    Edit to about me

--- src/components/Home/HomeAboutMe.js
+++ src/components/Home/HomeAboutMe.js
@@ -8,7 +8,7 @@ const HomeAboutMe = () => {
             <h1>About Me</h1>
 
             <p>
-                I am an aspiring developer who currently works a full time job in inventory and logistics. I have been with my current employer for 15 years and have peaked at performance and growth within the organization. I've chosen a new career path in computer programming with a focus on web development because I enjoy the challenging and creative outlet.
+                I am an aspiring developer who currently works a full time job in inventory and logistics. I have been with my current employer for 16 years and have peaked at performance and growth within the organization. I've chosen a new career path in computer programming with a focus on web development because I enjoy the challenging and creative outlet.
             </p>
 
         </div>
