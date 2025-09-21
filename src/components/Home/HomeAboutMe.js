346eabdeacca8f9f5c7c14e1b05a35352fab0d99
JonGudenzi <jongudenzi@gmail.com>
2025-09-21 11:05:18 -0400

    Pushing new changes to about me section

--- src/components/Home/HomeAboutMe.js
+++ src/components/Home/HomeAboutMe.js
@@ -3,12 +3,18 @@ import './HomeAboutMe.css'
 
 const HomeAboutMe = () => {
     return (
+
         <div className='aboutMe'>
 
             <h1>About Me</h1>
 
             <p>
-                I am an aspiring developer who currently works a full time job in inventory and logistics. I have been with my current employer for 16 years and have peaked at performance and growth within the organization. I've chosen a new career path in computer programming with a focus on web development because I enjoy the challenging and creative outlet.
+                I'm an aspiring web developer with nearly 20 years of experience in inventory, logistics, IT, purchasing, and warehouse management. After reaching the top of my career in operations, I've chosen to take a new path and pursue my long-standing interest in computer programming.
+                <br>
+                </br>
+            </p>
+            <p>
+                I enjoy web development because it combines challenge and creativity. I love building solutions that make a real impact. My background has taught me to think critically, adapt quickly, and solve problems across many areas of operations. These are skills that I now bring into my journey as a developer.
             </p>
 
         </div>
